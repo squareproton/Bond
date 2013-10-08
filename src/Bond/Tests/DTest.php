547438aac186e9060dd821062e25f1dfd70235eb
Peter Tuthill <squareproton@gmail.com>
2013-10-08 08:20:31 +0000

    WIP language agnositic ref-like parser. Removed uberdebug.

--- src/Bond/Tests/DTest.php
+++ src/Bond/Tests/DTest.php
@@ -3,14 +3,21 @@
 namespace Bond\Tests;
 
 use Bond\D;
+use Bond\D\RHtmlSpanFormatter;
+use Bond\D\LanguageAgnosticParser;
+
+class TSomething extends \stdclass
+{
+    public function whatever () {}
+
+}
 
 // class DTest extends \Bond\Normality\Tests\NormalityProvider
 class DTest extends \PHPUnit_Framework_Testcase
 {
-    public function testSomething()
-    {
 
-        d("fuck");
+    public function testLangAgnosticRefParser()
+    {
 
         $d = new D(
             '192.168.2.17',
@@ -22,9 +29,72 @@ class DTest extends \PHPUnit_Framework_Testcase
             ]
         );
 
-        $d( $this );
-        $d->clear();
-        $d( $this );
+
+        \ref::config('stylePath', false);
+        \ref::config('scriptPath', false);
+
+        $lap = new LanguageAgnosticParser( new RHtmlSpanFormatter() );
+
+        $data = array(
+            'handler' => 'php-ref',
+            'args' => array(
+                null,
+                []
+            )
+        );
+
+
+        $data['args'][0] = $lap->query($this->getNull());
+        $d->makeUberRequest($data);
+        $d(null);
+
+        $data['args'][0] = $lap->query($this->getScalarNumeric());
+        $d->makeUberRequest($data);
+        $d(3);
+
+        $data['args'][0] = $lap->query($this->getScalarString());
+        $d->makeUberRequest($data);
+        $d("hello world");
+
+        $data['args'][0] = $lap->query($this->getSimpleObject());
+        $d->makeUberRequest($data);
+        $d(new TSomething());
+
+    }
+
+    public function testSomething()
+    {
+
+        $d = new D(
+            '192.168.2.17',
+            'unittest/pete',
+            [
+                'showMethods' => true,
+                'showPrivateMembers' => true,
+                'expLvl' => 2
+            ]
+        );
+
+
+        $data = array(
+            'handler' => 'tabularData',
+            'args' => [
+                [
+                    [0,1,2,3,4,5],
+                    [0,1,2,3,4,5],
+                    [0,1,2,3,4,5],
+                    [0,1,2,3,4,5],
+                    [0,1,2,3,4,5],
+                ]
+            ]
+        );
+
+        $d->makeUberRequest($data);
+
+
+        return;
+
+        $d("fcuk");
 
         $d->syntaxHighlight( <<<SQL
             SELECT
@@ -35,5 +105,81 @@ class DTest extends \PHPUnit_Framework_Testcase
                 something = true
 SQL
         );
+
+        return;
+
+        $d->syntaxHighlight( <<<'SQL'
+var $spanner = '23'
+SQL
+            , 'javascript'
+        );
+
+
+        return;
+
+        $d( $this );
+        $d("fuck");
+
     }
+
+    private function getNull()
+    {
+        $output = "null";
+        return json_decode( $output, true );
+    }
+
+    private function getScalarNumeric()
+    {
+        $output = <<<JSON
+        {
+            "scalar": 3
+        }
+JSON;
+        return json_decode( $output, true );
+    }
+
+    private function getScalarString()
+    {
+        $output = <<<JSON
+        {
+            "scalar": "hello world"
+        }
+JSON;
+        return json_decode( $output, true );
+    }
+
+    private function getSimpleObject()
+    {
+        $output = <<<JSON
+        {
+            "methods": {
+                "getClass": [],
+                "registerNatives": [],
+                "clone": [],
+                "equals": [
+                    "java.lang.Object"
+                ],
+                "notify": [],
+                "hashCode": [],
+                "finalize": [],
+                "toString": [],
+                "wait": [
+                    "long",
+                    "int"
+                ],
+                "notifyAll": []
+            },
+            "static": {},
+            "constants": {},
+            "class": [
+                "java.lang.Object",
+                "java.lang.Object"
+            ],
+            "properties": {}
+        }
+JSON;
+        return json_decode( $output, true );
+    }
+
+
 }
